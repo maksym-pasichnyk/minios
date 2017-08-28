99504a8849d5fe077a8c87f7d7050a1264d61f10
Max Pasichnyk <maximmkaaa@gmail.com>
2017-08-29 01:58:34 +0300

    Update io.h

--- source/io.h
+++ source/io.h
@@ -1,13 +1,15 @@
 #include "stdlib.h"
 
 static inline uint8_t inb(uint16_t port) {
-    uint8_t ret;
-    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
+	uint8_t ret;
+	asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+	return ret;
 }
+
 static inline void outb(uint16_t port, uint8_t val) {
-  asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+	asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
 }
+
 static inline void io_wait(void) {
-  asm volatile ("outb %%al, $0x80" : : "a"(0));
+	asm volatile ("outb %%al, $0x80" : : "a"(0));
 }
